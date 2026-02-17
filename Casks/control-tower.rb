ac750e4f2d2d88c8ba2d552b3071106ba6ef953a
krishcdbry <krishcdbry@gmail.com>
2026-02-17 07:17:50 +0530

    fix: update cask sha256 for notarized build

--- Casks/control-tower.rb
+++ Casks/control-tower.rb
@@ -1,6 +1,6 @@
 cask "control-tower" do
   version "1.0.0-beta.1"
-  sha256 "79c2d769927f4f74fd39e98624cc29ae37181a1c1c5f4637c4fb4f3c6e639f71"
+  sha256 "e8562e42be9060c50cfb7846a2ecc44347e3a0f039d9116112388e825d21eb4c"
 
   url "https://github.com/krishcdbry/control-tower/releases/download/v#{version}/ControlTower-#{version}.zip",
       verified: "github.com/krishcdbry/control-tower/"
